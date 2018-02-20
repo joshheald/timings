002164c1c0aed1caf78947bf8f029ad8f25ad6ba
Josh Heald <joshheald@users.noreply.github.com>
2018-02-20 08:12:45 +0000

    tidied up presenter

--- Timings/TimerListPresenter.swift
+++ Timings/TimerListPresenter.swift
@@ -16,21 +16,27 @@ class TimerListPresenter {
     init(eventProvider: TimerListEventProvider) {
         self.eventProvider = eventProvider
         
-        timersItems = eventProvider.addTimerTapped.scan([], accumulator: { (oldValue, _) -> [TableItem] in
-            var newItems = oldValue
-            newItems.append(TableItem(reuseIdentifier: "TimerCell", title: "Totes a timer"))
-            return newItems
-        }).startWith([])
+        timersItems = eventProvider.addTimerTapped
+            .scan([], accumulator: { (oldValue, _) -> [TableItem] in
+                var newItems = oldValue
+                newItems.append(TableItem(reuseIdentifier: "TimerCell", title: "Totes a timer"))
+                return newItems
+            })
+            .startWith([])
             .debug("timers rows")
         
-        addTimerSection = eventProvider.viewDidLoad.map({ (_) -> SectionModel<String, TableItem> in
-            return SectionModel(model: "", items: [TableItem(reuseIdentifier: "AddTimerCell", title: "Add timer")])
-        }).debug("add timer")
+        addTimerSection = eventProvider.viewDidLoad
+            .map({ (_) -> SectionModel<String, TableItem> in
+                return SectionModel(model: "", items: [TableItem(reuseIdentifier: "AddTimerCell", title: "Add timer")])
+            })
+            .debug("add timer")
         
-        timersSection = timersItems.map({ (items) -> SectionModel<String, TableItem> in
-            return SectionModel(model: "Timers", items: items)
-        }).debug("timers section")
+        timersSection = timersItems
+            .map({ (items) -> SectionModel<String, TableItem> in
+                return SectionModel(model: "Timers", items: items)
+            })
+            .debug("timers section")
         
-        tableItems = Observable.combineLatest([timersSection, addTimerSection]).debug("merged items")
+        tableItems = Observable.combineLatest([timersSection, addTimerSection])
     }
 }
